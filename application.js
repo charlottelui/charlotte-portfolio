61d47896dd5c824c9e0879dd6a2c1bd646c8ac74
Charlotte Lui <charlotteytlui@gmail.com>
2020-03-19 17:25:36 +0200

    created filter for projects

--- application.js
+++ application.js
@@ -12,4 +12,35 @@ const initUpdateNavbarOnScroll = () => {
   }
 }
 
+const filterProject = () => {
+  const activeProject = document.querySelector('.project-select.active')
+  document.querySelectorAll('.project').forEach((project) => {
+    if (project.dataset.projectName === activeProject.dataset.projectName) {
+      project.classList.remove ('d-none')
+    } else {
+      project.classList.add('d-none')
+    }
+  })
+}
+
+const toggleProject = () => {
+  const buttons = document.querySelectorAll('.project-select')
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((b) => {
+        b.classList.remove('active')
+      })
+      button.classList.add('active')
+      filterProject()
+    })
+  })
+}
+
+const initFilter = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    toggleProject();
+  })
+}
+
+initFilter();
 initUpdateNavbarOnScroll();
